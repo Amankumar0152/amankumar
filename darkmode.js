2d2165edb78c6aae4bc218328de888d47ad0742b
Amankumar0152 <amankumarmaurya93@gmail.com>
2024-03-06 16:08:54 +0530

    added dark mode button

---
+++ darkmode.js
@@ -0,0 +1,16 @@
+
+let DarkBtn = document.querySelector(".btn");
+let body = document.querySelector("body");
+let currmode = "light";
+
+DarkBtn.addEventListener("click", ()=>{
+    if(currmode === "light"){
+        currmode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        currmode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+})
